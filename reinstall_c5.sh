9daf3cb22fac91d79b27b77247f2e5bb5abf48a2
bloorq@gmail.com <bloorq@gmail.com>
2019-06-14 20:37:30 +0100

    Chromium updater script

--- reinstall_c5.sh
+++ reinstall_c5.sh
@@ -4,17 +4,21 @@
 REPO_PATH=/Library/WebServer/Documents/concrete5
 
 echo -e "\n[+] Reinstalling local concrete5 instance..."
-cd $REPO_PATH
 
-echo -e "[+] updating files from GitHub repository..."
-git pull origin develop
+if [ -d $REPO_PATH ]
+then
+    cd $REPO_PATH
 
-echo -e "[+] removing temporary files..."
-rm -rf application/cache/*
-rm -rf application/files/*
+    echo -e "[+] updating files from GitHub repository..."
+    git pull origin develop
 
-echo -e "[+] deleting database.php..."
-cd $REPO_PATH/concrete/config
-rm -f database.php
+    echo -e "[+] removing temporary files..."
+    rm -rf $REPO_PATH/application/cache/*
+    rm -rf $REPO_PATH/application/files/*
 
-echo -e "\n[+] DONE"
+    echo -e "[+] deleting database.php..."
+    cd $REPO_PATH/concrete/config
+    rm -f database.php
+
+    echo -e "\n[+] DONE"
+fi
---
+++ update-chromium.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove previous downloaded .zip archive
+rm -rf ~/tmp/chromium_latest.zip
+
+# download newest Chromium build
+wget https://download-chromium.appspot.com/dl/Mac?type=snapshots -O ~/tmp/chromium_latest.zip
+
+# unizp...
+unzip ~/tmp/chromium_latest.zip
+
+# ...remove current Chromium app from Apps...
+rm -rf /Applications/Chromium.app
+
+# ...and finally copy newest build into Apps
+mv -f ~/tmp/chrome-mac/Chromium.app /Applications
