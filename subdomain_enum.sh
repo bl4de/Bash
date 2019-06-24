17ac540fa7319b22107aa12cdef2cdc75c1517a1
bloorq@gmail.com <bloorq@gmail.com>
2019-06-24 22:59:36 +0100

    subdomain_enum.sh - iterate over list of domain(s); run denumerator on final list

--- subdomain_enum.sh
+++ subdomain_enum.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-DOMAIN=$1
+# list of domains - text file, one domain in single line
+DOMAINS=$1
 
-sublister -d $DOMAIN -o $DOMAIN.sublister
-amass enum -brute -min-for-recursive 1 -d $DOMAIN -o $DOMAIN.amass
+if ! [ -d 'domains' ]; then
+    mkdir domains
+fi
 
-cat $DOMAIN.* > $DOMAIN.all
-sort -u -k 1 $DOMAIN.all > $DOMAIN
+for DOMAIN in $(cat $DOMAINS)
+do
+    sublister -d $DOMAIN -o domains/$DOMAIN.sublister
+    amass enum -brute -min-for-recursive 1 -d $DOMAIN -o domains/$DOMAIN.amass
+    
+    if [ -s domains/$DOMAIN.sublister ] && [ -s domains/$DOMAIN.amass ]; then
+        cat domains/$DOMAIN.* > domains/$DOMAIN.all
+        sort -u -k 1 domains/$DOMAIN.all > domains/$DOMAIN
+    fi
+    rm -f domains/$DOMAIN.*
+done
 
-rm -f $DOMAIN.*
+# concatenate and sort all domains from the target
+cat domains/*.* > domains/domains.all
+sort -u -k 1 domains/domains.all > domains/domains.final
 
-echo -e "\n[+} DONE. Found $(wc -l $DOMAIN) unique subdomains"
+rm -f domains/domains.all
+echo -e "\n[+} DONE. Found $(wc -l domains/domains.final) unique subdomains"
+
+# run denumerator on the domains/domains.final output file
+denumerator -f domains/domains.final
+
+echo -e "\n[+} DONE."
+
 
