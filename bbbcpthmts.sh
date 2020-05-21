9c89261d8c5c4cd056444f391ad9c08d09086b47
bloorq@gmail.com <bloorq@gmail.com>
2020-05-21 13:35:56 +0100

    bbbcpthmts.sh ssh_to_john()

--- bbbcpthmts.sh
+++ bbbcpthmts.sh
@@ -25,13 +25,22 @@ rockyou_john() {
     echo -e "[+] Running john with rockyou dictionary against $1 of type $2"
     echo > /Users/bl4de/hacking/tools/jtr/run/john.pot
     if [[ -n $2 ]]; then
-        format=$2
+        /Users/bl4de/hacking/tools/jtr/run/john --wordlist=/Users/bl4de/hacking/dictionaries/rockyou.txt "$1" --format="$2"
         elif [[ -z $2 ]]; then
-        format=""
+        /Users/bl4de/hacking/tools/jtr/run/john --wordlist=/Users/bl4de/hacking/dictionaries/rockyou.txt "$1"
     fi
-    /Users/bl4de/hacking/tools/jtr/run/john --wordlist=/Users/bl4de/hacking/dictionaries/rockyou.txt "$1" --format="$format"
     cat /Users/bl4de/hacking/tools/jtr/run/john.pot
 }
+
+# converts id_rsa to JTR format for cracking SSH key
+ssh_to_john() {
+    echo -e "[+] Converting SSH id_rsa key to JTR format to crack it"
+    python /Users/bl4de/hacking/tools/jtr/run/sshng2john.py "$1" > "$1".hash
+    echo -e "[+] We have a hash.\n"
+    echo -e "[+] Let's now crack it!"
+    rockyou_john "$1".hash
+}
+
 
 cmd=$1
 case "$cmd" in
@@ -44,12 +53,16 @@ case "$cmd" in
     rockyou_john)
         rockyou_john "$2" "$3"
     ;;
+    ssh_to_john)
+        ssh_to_john "$2"
+    ;;
     *)
         echo -e "Usage: $0 {cmd} {arg1} {arg2}...{argN}\n"
         echo -e "Available commands:\n"
         echo -e "\tfull_nmap_scan [IP]\t\t -> nmap -p- to enumerate ports + -sV -sC -A on found open ports"
         echo -e "\thttp_server [PORT]\t\t -> runs HTTP server on [PORT] TCP port"
         echo -e "\trockyou_john [TYPE] [HASHES]\t -> runs john+rockyou against [HASHES] file with hashes of type [TYPE]"
+        echo -e "\tssh_to_john [ID_RSA]\t\t -> id_rsa to JTR SSH hash file for SSH key password cracking"
         echo -e "\nHack The Planet!"
     ;;
 esac
