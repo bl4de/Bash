e14e23828837842e5d4648f21135d44efd940dc5
bloorq@gmail.com <bloorq@gmail.com>
2020-05-23 13:49:07 +0100

    bbbcpthmts.sh - npm_scan for npm modules static analysis with nodestructor and semgrep

--- bbbcpthmts.sh
+++ bbbcpthmts.sh
@@ -3,21 +3,22 @@
 # bl4de's BugBounty/CTF/PenTest/Hacking multi-tool suite -> bbbcpthmts :D
 # collection of various wrappers, multi-commands, tips&tricks, shortcuts etc.
 # CTX: bl4de@wearehackerone.com
+HACKING_HOME="/Users/bl4de/hacking"
 
 # runs -p- against IP; then -sV -sC -A against every open port found
 full_nmap_scan() {
     echo -e "[+] Running full nmap scan against $1..."
     echo -e " -> search all open ports..."
-    ports=$(nmap -p- --min-rate=1000 $1 | grep open | cut -d'/' -f 1 | tr '\n' ',')
+    ports=$(nmap -p- --min-rate=1000 "$1" | grep open | cut -d'/' -f 1 | tr '\n' ',')
     echo -e " -> run version detection + nse scripts against $ports..."
-    nmap -p$ports -sV -sC -A -Pn -n $1 -oN ./$1.log
+    nmap -p"$ports" -sV -sC -A -Pn -n "$1" -oN ./"$1".log
     echo -e "[+] Done!"
 }
 
 # runs Python 3 built-in HTTP server on [PORT]
 http_server() {
     echo -e "[+] Running Simple HTTP Server in current directory on port $1"
-    python3 -m http.server $1
+    python3 -m http.server "$1"
 }
 
 # runs john with rockyou.txt against hash type [FORMAT] and file [HASHES]
@@ -41,6 +42,19 @@ ssh_to_john() {
     rockyou_john "$1".hash
 }
 
+# static code analysis of npm module installed in ~/node_modules
+# with nodestructor and semgrep
+npm_scan() {
+    echo -e "[+] Starting static code analysis of $1 module with nodestructor and semgrep..."
+    nodestructor -r ~/node_modules/"$1" --verbose --skip-test-files
+    semgrep --lang javascript --config "$HACKING_HOME"/tools/semgrep-rules/contrib/nodejsscan/ "$HOME"/node_modules/"$1"/*.js
+    exitcode=$(ls "$HOME"/node_modules/"$1"/*/ >/dev/null 2>&1)
+    if [ "$exitcode" == 0 ]; then
+        semgrep --lang javascript --config "$HACKING_HOME"/tools/semgrep-rules/contrib/nodejsscan/ "$HOME"/node_modules/"$1"/**/*.js
+    fi
+    echo -e "\n\n[+]Done."
+}
+
 
 cmd=$1
 case "$cmd" in
@@ -56,13 +70,21 @@ case "$cmd" in
     ssh_to_john)
         ssh_to_john "$2"
     ;;
+    npm_scan)
+        npm_scan "$2"
+    ;;
     *)
         echo -e "Usage: $0 {cmd} {arg1} {arg2}...{argN}\n"
         echo -e "Available commands:\n"
+        echo -e ":: RECON ::"
         echo -e "\tfull_nmap_scan [IP]\t\t -> nmap -p- to enumerate ports + -sV -sC -A on found open ports"
+        echo -e "\n:: TOOLS ::"
         echo -e "\thttp_server [PORT]\t\t -> runs HTTP server on [PORT] TCP port"
+        echo -e "\n:: PASSWORDS CRACKIN' ::"
         echo -e "\trockyou_john [TYPE] [HASHES]\t -> runs john+rockyou against [HASHES] file with hashes of type [TYPE]"
         echo -e "\tssh_to_john [ID_RSA]\t\t -> id_rsa to JTR SSH hash file for SSH key password cracking"
+        echo -e "\n:: STATIC CODE ANALYSIS ::"
+        echo -e "\tnpm_scan [MODULE_NAME]\t\t -> static code analysis of MODULE_NAME npm module with nodestructor and semgrep"
         echo -e "\nHack The Planet!"
     ;;
 esac
